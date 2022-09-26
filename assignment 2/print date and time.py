a93cf8f953511d70fad878b0bc030d1463be875b
asiffaizi <113881612+asiffaizi@users.noreply.github.com>
2022-09-26 12:00:06 +0530

    Add files via upload

---
+++ assignment 2/print date and time.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+datetime=datetime.today()
+print(datetime)
+d1=datetime.strftime("%d-%m-%Y and %I:%M %p")
+print(d1)
---
+++ assignment 2/print value,its type and id.py
@@ -0,0 +1,9 @@
+w=10
+x=True
+y="Faizi"
+z=5.2
+print(type,(id(w)))
+print(type,(id(x)))
+print(type(y))
+print(type(z))
+
